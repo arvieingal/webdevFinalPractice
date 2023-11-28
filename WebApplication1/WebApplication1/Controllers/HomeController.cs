192f744ac5fe28d55cd50d6aaf2890b10dfa8d8b
arvie <arvie@gmail.com>
2023-11-28 19:36:07 +0800

    second commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult InputData()
+        {
+            ViewBag.Message = "Input your data";
+
+            return View();
+        }
+
+        public ActionResult AddUserToDatabase(FormCollection fc)
+        {
+            String firstName = fc["firstname"];
+            String lastName = fc["lastname"];
+            String email = fc["email"];
+            String diko = fc["password"];
+
+            user use = new user();
+            use.firstName = firstName;
+            use.lastName = lastName;
+            use.email = email;
+            use.password = diko;
+            use.roleId = 1;
+
+            remindersiteEntities fe = new remindersiteEntities();
+            fe.users.Add(use);
+            fe.SaveChanges();
+
+            //insert the code that will save these information to the DB
+
+            return RedirectToAction("InputData");
+        }
+
+        //public ActionResult EdituserFromDatabase(FormCollection fc)
+        //{
+            
+        //}
+    }
+}
